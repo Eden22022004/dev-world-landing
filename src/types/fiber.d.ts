74fb43d4611fe803088810a0d01ae6d737faa43f
Eden22022004 <92798869+Eden22022004@users.noreply.github.com>
2025-11-09 23:19:09 +0200

    Update fiber.d.ts

--- src/types/fiber.d.ts
+++ src/types/fiber.d.ts
@@ -1,19 +1,18 @@
-// src/types/fiber.d.ts
 import '@react-three/fiber';
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      primitive: any
-      group: any
-      mesh: any
-      boxGeometry: any
-      sphereGeometry: any
-      meshStandardMaterial: any
-      ambientLight: any
-      directionalLight: any
+      primitive: any;
+      group: any;
+      mesh: any;
+      boxGeometry: any;
+      sphereGeometry: any;
+      meshStandardMaterial: any;
+      ambientLight: any;
+      directionalLight: any;
     }
   }
 }
 
-export {}
+export {};
