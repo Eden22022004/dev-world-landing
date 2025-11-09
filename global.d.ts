ea5cf7252db32b1cf4552410d38c7989977f2a6c
Eden22022004 <92798869+Eden22022004@users.noreply.github.com>
2025-11-09 22:52:28 +0200

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="@react-three/fiber" />
-
-// Це каже TypeScript, що JSX елементи від @react-three/fiber валідні
-declare namespace JSX {
-  interface IntrinsicElements {
-    // Підключаємо все, що підтримує @react-three/fiber
-    // Це робиться через імпорт JSX типів із бібліотеки
-    // Нічого додатково не потрібно
-  }
-}
