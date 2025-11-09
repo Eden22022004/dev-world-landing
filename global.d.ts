b89f94f8a3747c84a2b98885bf35289bb8b6eb2e
Eden22022004 <92798869+Eden22022004@users.noreply.github.com>
2025-11-09 22:35:17 +0200

    Create global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,16 @@
+import '@react-three/fiber'
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      // Тепер JSX знає про три-вимірні елементи
+      group: ReactThreeFiber.Object3DNode<THREE.Group, typeof THREE.Group>
+      mesh: ReactThreeFiber.Object3DNode<THREE.Mesh, typeof THREE.Mesh>
+      boxGeometry: ReactThreeFiber.Object3DNode<THREE.BoxGeometry, typeof THREE.BoxGeometry>
+      sphereGeometry: ReactThreeFiber.Object3DNode<THREE.SphereGeometry, typeof THREE.SphereGeometry>
+      meshStandardMaterial: ReactThreeFiber.Object3DNode<THREE.MeshStandardMaterial, typeof THREE.MeshStandardMaterial>
+      ambientLight: ReactThreeFiber.Object3DNode<THREE.AmbientLight, typeof THREE.AmbientLight>
+      directionalLight: ReactThreeFiber.Object3DNode<THREE.DirectionalLight, typeof THREE.DirectionalLight>
+    }
+  }
+}
