ae9abd1a4f9248ea94ff2c0c1f4664fd99c79f39
Eden22022004 <92798869+Eden22022004@users.noreply.github.com>
2025-11-10 00:07:36 +0200

    Create fiber.d.ts

---
+++ src/types/fiber.d.ts
@@ -0,0 +1,15 @@
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      group: any
+      mesh: any
+      boxGeometry: any
+      sphereGeometry: any
+      meshStandardMaterial: any
+      ambientLight: any
+      directionalLight: any
+    }
+  }
+}
+
+export {}
