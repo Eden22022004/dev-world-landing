07994fc9d6041afd6a3b55a3ac85a3c6a5f81539
Eden22022004 <92798869+Eden22022004@users.noreply.github.com>
2025-11-10 09:42:18 +0200

    Update ThreeScene.tsx

--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -6,35 +6,24 @@ import { OrbitControls, Stars } from '@react-three/drei'
 import * as THREE from 'three'
 
 function FloatingBoxes() {
-  const groupRef = useRef<THREE.Group>(null)
+  const groupRef = useRef<THREE.Group>(null!)
 
   useFrame((state) => {
-    if (groupRef.current) {
-      groupRef.current.rotation.y += 0.002
-      groupRef.current.rotation.x = Math.sin(state.clock.elapsedTime / 6) * 0.1
-    }
+    groupRef.current.rotation.y += 0.002
+    groupRef.current.rotation.x = Math.sin(state.clock.elapsedTime / 6) * 0.1
   })
 
   return (
-    // @ts-ignore
     <group ref={groupRef}>
-      // @ts-ignore
       <mesh position={[1.2, 0.3, -1.2]}>
-        // @ts-ignore
         <boxGeometry args={[0.9, 0.9, 0.9]} />
-        // @ts-ignore
         <meshStandardMaterial color="#7C3AED" metalness={0.6} roughness={0.1} />
-      // @ts-ignore
       </mesh>
-      // @ts-ignore
+
       <mesh position={[-1.2, -0.4, -0.6]}>
-        // @ts-ignore
         <sphereGeometry args={[0.6, 32, 32]} />
-        // @ts-ignore
         <meshStandardMaterial color="#06B6D4" metalness={0.2} roughness={0.3} />
-      // @ts-ignore
       </mesh>
-    // @ts-ignore
     </group>
   )
 }
